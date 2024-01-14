54c61df98fa407546b9446a9645f2f27143917ec
vietnh.161 <vietnh.161@gmail.com>
2024-01-15 01:06:47 +0700

    test router

--- src/layout/Sidebar/Index.tsx
+++ src/layout/Sidebar/Index.tsx
@@ -1,10 +1,16 @@
 import { FunctionComponent } from "react";
 import "./Style.scss";
+import { Link } from "react-router-dom";
 
 interface SidebarProps {}
 
 const Sidebar: FunctionComponent<SidebarProps> = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <Link className="sidebar__link" to="project/board">board</Link>
+      <Link className="sidebar__link" to="project/setting">setting</Link>
+    </div>
+  );
 };
 
 export default Sidebar;
