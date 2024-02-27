8b04fc8b0a35993b6474fb04379bb48627081d3d
vietnh.161 <vietnh.161@gmail.com>
2024-02-28 01:04:00 +0700

    clean up

--- src/pages/CreateIssueModal/index.tsx
+++ src/pages/CreateIssueModal/index.tsx
@@ -5,14 +5,12 @@ import {
   Formik,
   FormikConfig,
   FormikProps,
-  useFormik,
 } from "formik";
-import * as Yup from "yup";
-import ReactQuill from "react-quill";
-import Modal from "../../shared/components/Modal";
 import "react-quill/dist/quill.snow.css";
-import "./Style.scss";
+import * as Yup from "yup";
 import Input from "../../shared/components/Input";
+import Modal from "../../shared/components/Modal";
+import "./Style.scss";
 
 interface CreateIssueModalProps {
   open: boolean;
@@ -39,103 +37,44 @@ const CreateIssueModal = (props: CreateIssueModalProps) => {
       alert(values);
     },
   };
-  const createForm = useFormik(config);
   return (
     <Modal open={open} showCloseButton={false} onClose={onClose}>
       <div className="create-issue-form">
         <div className="create-issue-form__title">Create issue</div>
-        <form onSubmit={createForm.handleSubmit}>
-          <div className="create-issue-form__body">
-            <div className="divider"></div>
-            <Input
-              componentClass="m-t-20"
-              label="Short Summary"
-              description="Concisely summarize the issue in one or two sentences."
-              id="shortSummary"
-              name="shortSummary"
-              type="text"
-              field={createForm.getFieldProps("shortSummary")}
-              errorMessage={
-                createForm.touched.shortSummary
-                  ? createForm.getFieldMeta("shortSummary").error
-                  : ""
-              }
-            ></Input>
-            {/* <div className="create-issue-form__field">
-              <label className="field__label" htmlFor="description">
-                Description
-              </label>
-              <div className="field__text-editor">
-                <ReactQuill
-                  theme="snow"
-                  value={createForm.values.description}
-                  onChange={(value) =>
-                    createForm.setFieldValue("description", value)
-                  }
-                />
-              </div>
-              <div className="field__description">
-                Describe the issue in as much detail as you'd like.
-              </div>
-              {createForm.touched.description &&
-              createForm.errors.description ? (
-                <div className="field__error-message">
-                  {createForm.errors.description}
+        <div className="create-issue-form__body">
+          <Formik {...config}>
+            {(props: FormikProps<any>) => (
+              <Form>
+                <Field name="shortSummary">
+                  {({ field, meta }: FieldProps<any>) => (
+                    <Input
+                      componentClass="m-t-20"
+                      label="Short Summary"
+                      description="Concisely summarize the issue in one or two sentences."
+                      id="shortSummary"
+                      name="shortSummary"
+                      type="text"
+                      field={field}
+                      errorMessage={meta.touched ? meta.error : ""}
+                    ></Input>
+                  )}
+                </Field>
+                <div className="create-issue-form__submit">
+                  <button className="field__submit-button" type="submit">
+                    Submit
+                  </button>
+                  <button
+                    className="field__cancel-button"
+                    type="button"
+                    onClick={onClose}
+                  >
+                    Cancel
+                  </button>
                 </div>
-              ) : null}
-            </div> */}
-          </div>
-
-          <div className="create-issue-form__footer">
-            <button className="field__submit-button" type="submit">
-              Submit
-            </button>
-            <button
-              className="field__cancel-button"
-              type="button"
-              onClick={onClose}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-
-        <div className="create-issue-form__title">Another form</div>
-        <Formik {...config}>
-          {(props: FormikProps<any>) => (
-            <Form>
-              <Field name="shortSummary">
-                {({
-                  field,
-                  form: { touched, errors },
-                  meta,
-                }: FieldProps<any>) => (
-                  <Input
-                    componentClass="m-t-20"
-                    label="Short Summary"
-                    description="Concisely summarize the issue in one or two sentences."
-                    id="shortSummary"
-                    name="shortSummary"
-                    type="text"
-                    field={field}
-                    errorMessage={meta.touched ? meta.error : ""}
-                  ></Input>
-                )}
-              </Field>
-
-              <button className="field__submit-button" type="submit">
-                Submit
-              </button>
-              <button
-                className="field__cancel-button"
-                type="button"
-                onClick={onClose}
-              >
-                Cancel
-              </button>
-            </Form>
-          )}
-        </Formik>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </Modal>
   );
